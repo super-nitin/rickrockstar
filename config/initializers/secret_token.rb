aadc2739257456a3914fbcc436737186462a551d
Nitin Gupta <git@radpid.com>
2010-12-29 10:33:43 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = '3e0b9d5c0629d81dfd9de743bb5f89a7c1e2d2d57d8a89c1256dd53d3f97fcf8a46e2eeebc1992059cee230966b7db6876fadfeb3893d437f7417eea6153920e'
